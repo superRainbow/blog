fb77415c3b7332876b5fc434e10b34bd55849173
rainbow <g901612002@hotmail.com>
2021-04-07 15:54:00 +0800

    blog init

---
+++ .vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+    title: '金魚腦的學習筆記',
+    description: '資訊爆炸的年代，邊學邊記是標配',
+    base: '/blog/',
+    dest: 'dist',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }],
+        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+    ],
+    locales: {
+        '/': {
+            lang: 'zh-TW',
+        },
+    },
+    theme: 'reco',
+    themeConfig: {
+        nav: [
+            { text: 'TimeLine', icon: 'reco-date', link: '/timeline/' },
+            {
+                text: 'Rainbow',
+                icon: 'reco-account',
+                items: [
+                    { text: 'About Me', icon: 'reco-message', link: 'https://superrainbow.github.io/website/#/about' },
+                    { text: 'GitHub', link: 'https://github.com/superrainbow', icon: 'reco-github' },
+                ],
+            },
+        ],
+        type: 'blog',
+        blogConfig: {
+            category: {
+                location: 2, // 在导航栏菜单中所占的位置，默认2
+                text: 'Category', // 默认 “分类”
+            },
+            tag: {
+                location: 3, // 在导航栏菜单中所占的位置，默认3
+                text: 'Tag', // 默认 “标签”
+            },
+        },
+        logo: '/logo.png',
+        // 搜索设置
+        search: true,
+        searchMaxSuggestions: 10,
+        author: '金魚腦',
+        authorAvatar: '/logo.jpg',
+        startYear: '2021',
+    },
+    markdown: {
+        lineNumbers: true,
+    },
+    plugins: ['@vuepress/medium-zoom', '@vuepress/back-to-top'],
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+---
+home: true
+heroText: 金魚腦的學習筆記
+tagline: 資訊爆炸的年代，邊學邊記是標配
+# heroImage: /hero.png
+# heroImageStyle: {
+#   maxWidth: '600px',
+#   width: '100%',
+#   display: block,
+#   margin: '9rem auto 2rem',
+#   background: '#fff',
+#   borderRadius: '1rem',
+# }
+bgImageStyle: { height: '200px', background-position: top center !important }
+isShowTitleInHome: false
+actionText: Guide
+actionLink: /views/other/guide
+---
